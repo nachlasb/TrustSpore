aec7fd45b7b9de73bb4331649ca243a2e2180b7f
Ben <nachlasb@vt.edu>
2025-05-14 01:47:20 -0700

    added new images

--- client/src/lib/images.ts
+++ client/src/lib/images.ts
@@ -7,12 +7,13 @@ export const labImage = "/client/public/images/labimage.jpg"; // Path to the ima
 
 
 // DNA testing process section
-export const sporeCollectionImage = "https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600";
+export const sporeCollectionImage = "https://www.milkwood.net/wp-content/uploads/2014/01/screen-shot-2014-01-01-at-4-09-35-pm.png";
+//export const sporeCollectionImage = "https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600";
 export const labAnalysisImage = "https://images.unsplash.com/photo-1581093450021-4a7360e9a6b5?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&h=600";
 
 // Marketplace section
 export const blueOysterImage = "https://pixabay.com/get/gd1f02ead889c17bfa321c41743554d077ea019d9a0db7155e3da58d5f5bc879bf47208d541794d814a56c4e2af16684b29d570545d6f95714a0bb2d0a61faf48_1280.jpg";
 //export const lionsManeImage = "https://pixabay.com/get/g1b6791d1082f4b3083c079adf4edc0699a34811230aa3e7461b0c4e0c23c00411980b4abdeeb4dc10cca474a024a7c262088a0a541f5dfd193c87222b312fb0f_1280.jpg";
 export const lionsManeImage = "https://familyhealthadvocacy.com/wp-content/uploads/2019/07/Depositphotos_171059682_xl-2015-min.jpg";
-export const reishiImage = "https://pixabay.com/get/g018d3b1174e360a9393b4d66d05be17a44974a7f6459401f4c657d30b9e11f478d3c19aa5005cd1ff68e74ff612e0de3cc243b458132297f5afd1d7b2c9b782c_1280.jpg";
-export const mushroomCollectionImage = "https://pixabay.com/get/g0bdd64ac680761df03aca378c5a5f4f2c57335293b9a468d45cf4dd0fda920bc0560c121868f0937f8f83060709696a3a5be5104dea5a6a6fe54a0ab959137ef_1280.jpg";
+export const reishiImage = "https://thumbs.dreamstime.com/z/harvesting-culture-antler-reishi-mushroom-bottle-image-harvesting-culture-antler-reishi-mushroom-bottle-319479563.jpg?w=576";
+export const mushroomCollectionImage = "https://www.adamsmushrooms.com/wp-content/uploads/2014/04/20161203_084531.jpg";
